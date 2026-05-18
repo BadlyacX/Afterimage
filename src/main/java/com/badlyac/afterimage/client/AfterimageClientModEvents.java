714e521e04f5ef17c6eaaa1751bd6cce1f5a3612
Badly <gary970509@gmail.com>
2026-05-18 22:11:33 +0800

    added Palemimic spawn egg

---
+++ src/main/java/com/badlyac/afterimage/client/AfterimageClientModEvents.java
@@ -0,0 +1,25 @@
+package com.badlyac.afterimage.client;
+
+import com.badlyac.afterimage.AfterimageMod;
+import com.badlyac.afterimage.monster.palemimic.PaleMimicRenderer;
+import com.badlyac.afterimage.registry.ModEntities;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(
+        modid = AfterimageMod.MOD_ID,
+        value = Dist.CLIENT,
+        bus = Mod.EventBusSubscriber.Bus.MOD
+)
+public class AfterimageClientModEvents {
+
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(
+                ModEntities.PALE_MIMIC.get(),
+                PaleMimicRenderer::new
+        );
+    }
+}
